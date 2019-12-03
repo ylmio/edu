8c5ea746860d763850c3675c560fa82accbf1892
ylm <yingleiming@outlook.com>
2019-12-03 17:51:23 +0800

    渲染index.html

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router({});
-
+//访问路由，访问后端界面
 router.get("/back",(req,res)=>{
+    //使用res.render来渲染 渲染back下面的index.html页面
     res.render("back/index.html")
 });
 
