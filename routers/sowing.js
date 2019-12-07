e2a6c12f3ee69be3a2e03681808ec0cb362790d3
ylm <yingleiming@outlook.com>
2019-12-07 21:24:21 +0800

    ..............

--- routers/sowing.js
+++ routers/sowing.js
@@ -9,7 +9,7 @@ router.get("/sowing/api/add",(req,res)=>{
     //操作数据库
     const sowing = new Sowing({
         //图片名称
-        image_title:"我是轮播图 ",
+        image_title:"我是轮播图",
         //图片地址
         image_url:"like.com/address.com",
         //跳转链接
