f52be1bc5190b923433e2e2534fd86811bde05b6
ylm <yingleiming@outlook.com>
2019-12-16 23:14:36 +0800

    使用post get请求完成轮播图的添加工作

--- routers/sowing.js
+++ routers/sowing.js
@@ -6,33 +6,33 @@ const router = express.Router({});
 * 往数据库中插入一条数据
 * */
 router.post("/sowing/api/add",(req,res)=>{
-    console.log(req.body);
-    res.send(req.body);
+    //1.获取数据
+    const body = req.body;
     //操作数据库
-    // const sowing = new Sowing({
-    //     //图片名称
-    //     image_title:"我是轮播图",
-    //     //图片地址
-    //     image_url:"like.com/address.com",
-    //     //跳转链接
-    //     image_link:"www.baidu.complete ",
-    //     //上架时间
-    //     s_time:"2019-11-12",
-    //     //下架时间
-    //     e_time:"2019-12-12",
-    // });
+    const sowing = new Sowing({
+        //图片名称
+        image_title:body.image_title,
+        //图片地址
+        image_url:body.image_url,
+        //跳转链接
+        image_link:body.image_link,
+        //上架时间
+        s_time:body.s_time,
+        //下架时间
+        e_time:body.e_time,
+    });
     //保存数据
-    // Sowing.save((err,result)=>{
-    //     //如果出错，返回错误信息
-    //     if(err){
-    //         throw err;
-    //     }
-    //     //否则，以json的形式将数据返回出去
-    //     res.json({
-    //         status:"200",
-    //         result:"添加轮播图成功"
-    //     });
-    // });
+    sowing.save((err,result)=>{
+        //如果出错，返回错误信息
+        if(err){
+            throw err;
+        }
+        //否则，以json的形式将数据返回出去
+        res.json({
+            status:"200",
+            result:"添加轮播图成功"
+        });
+    });
 });
 
 
