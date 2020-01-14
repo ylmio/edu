aca996b5f29a287b5df1f1b32b6254b6c0d6b0b4
ylm <yingleiming@outlook.com>
2020-01-14 15:18:40 +0800

    建立新页面，并生成后台管理元

--- routers/user.js
+++ routers/user.js
@@ -5,8 +5,15 @@ const router = express.Router({});
 
 
 /***********************数据接口API-start*******************************************/
+/*
+* 生成后台管理员
+*
+* */
+router.post('/user/api/add',(req,res,next)=>{
 
 
+
+});
 
 /***********************数据接口API-end*******************************************/
 
@@ -18,7 +25,19 @@ const router = express.Router({});
 router.get("/back/login",(req,res,next)=>{
     res.render("back/login.html");
 });
+
+router.get("/back/u_center",(req,res,next)=>{
+    res.render("back/user_center.html");
+});
+
+router.get("/back/u_set",(req,res,next)=>{
+    res.render("back/user_message.html");
+});
+
+router.get("/back/u_reset_pwd",(req,res,next)=>{
+    res.render("back/reset_pwd.html");
+});
 /***********************页面的路由-end*******************************************/
 
 
-export default router;
+export default router;//输出
