fdd5919d2e26dd090fbdb5305c4ccfac13341776
ylm <yingleiming@outlook.com>
2020-01-15 16:49:19 +0800

    session配置和数据库连接

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ import nunjucks from "nunjucks"
 import bodyParser from "./../middle_wares/body_parser"
 import errorLog from "./../middle_wares/error_log"
 
+//引入express-session
+import session from 'express-session'
+//引入connect-mongo用于express连接数据库存储session
+const mongoStore = required('connect-mongo')(session);
+
 //3.引入路由
 import indexRoute from "./../routers/index"
 import sowingRouter from "./../routers/sowing"
@@ -21,6 +26,23 @@ import userRouter from "./../routers/user"
 
 //2.创建服务器app
 const app = express();
+
+//使用session
+app.use(session({
+    secret: 'itlike',//加密字符串
+    name:'like_id',//返回客户端key的名称，默认为connect_sid
+    resave: false, //强制保存session,即使它没有变化
+    saveUninitialized: true, // 强制将未初始化的session存储。当新建一个session且未设定属性或值时，它就处于未初始化状态。在设定cookie前，这对于登陆验证，减轻服务器存储压力，权限控制是有帮助的，默认为true
+    cookie:{maxAge:1800000},
+    rolling:true,//在每次请求时进行设置cookie，将重置cookie过期时间
+    store: new MongoStore({
+        url: 'mongodb://127.0.0.1/college',//数据库地址
+        touchAfter: 24 * 3600 // 多长时间往数据库中更新存储一次，除了在会话数据上更改了某些数据外
+    })
+}));
+
+
+
 
 //2.1.配置公共资源访问路径
 app.use(express.static(config.publicPath));
